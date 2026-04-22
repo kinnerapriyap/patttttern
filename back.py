ae8bedc72e8beb1161d920929e3ed58953101b44
Kinnera Priya Putti <kinnerapriyap@gmail.com>
2026-04-22 11:40:14 +0200

    Fix back and front with darts

--- back.py
+++ back.py
@@ -5,18 +5,25 @@ from svg_rendering import render_svg
 def get_back_shapes():
     pts = build_points()
     return [
-        ("curve", pts["p20"], pts["p21"], 0.45),
-        ("line", pts["p21"], pts["p8"]),
-        ("line", pts["p8"], pts["p36"]),
-        ("line", pts["p33_2"], pts["p36"]),
-        ("line", pts["p32"], pts["p33_2"]),
+        ("curve", pts["p9"], pts["p1"], 0.45),
+        ("line", pts["p12_2"], pts["p9"]),
+        ("line", pts["p13"], pts["p12_2"]),
+        ("line", pts["p13"], pts["p12_1"]),
+        ("line", pts["p12_1"], pts["p11"]),
         ("french_curve", [pts["p11"], pts["p16"], pts["p14a"], pts["p32"], pts["p22a"], pts["p31"], pts["p30"]], 6.0),
-        ("line", pts["p27"], pts["p30"]),
-        ("line", pts["p27"], pts["p26"]),
-        ("line", pts["p20"], pts["p26"]),
+        ("line", pts["p32"], pts["p33_1"]),
+        ("line", pts["p33_1"], pts["p35"]),
+        ("line", pts["p35"], pts["p7"]),
+        ("line", pts["p7"], pts["p1"]),
+        ("line", pts["p17"], pts["p18_1"]),
+        ("line", pts["p17"], pts["p18_2"]),
+        ("line", pts["p18_1"], pts["p19"]),
+        ("line", pts["p18_2"], pts["p19"]),
 
-        ("dash", pts["p6"], pts["p33"]),
-        ("dash", pts["p3"], pts["p32"]),
+        ("dash", pts["p5"], pts["p33"]),
+        ("dash", pts["p2"], pts["p32"]),
+        ("dash", pts["p10"], pts["p11"]),
+        ("dash", pts["p17"], pts["p19"]),
     ]
 
 
--- front.py
+++ front.py
@@ -5,20 +5,23 @@ from svg_rendering import render_svg
 def get_front_shapes():
     pts = build_points()
     return [
-        ("curve", pts["p9"], pts["p1"], 0.45),
-        ("line", pts["p12_2"], pts["p9"]),
-        ("line", pts["p13"], pts["p12_2"]),
-        ("line", pts["p13"], pts["p12_1"]),
-        ("line", pts["p12_1"], pts["p11"]),
+        ("curve", pts["p20"], pts["p21"], 0.45),
+        ("line", pts["p21"], pts["p8"]),
+        ("line", pts["p8"], pts["p36"]),
+        ("line", pts["p33_2"], pts["p36"]),
+        ("line", pts["p32"], pts["p33_2"]),
         ("french_curve", [pts["p11"], pts["p16"], pts["p14a"], pts["p32"], pts["p22a"], pts["p31"], pts["p30"]], 6.0),
-        ("line", pts["p32"], pts["p33_1"]),
-        ("line", pts["p33_1"], pts["p35"]),
-        ("line", pts["p35"], pts["p7"]),
-        ("line", pts["p7"], pts["p1"]),
+        ("line", pts["p27"], pts["p30"]),
+        ("line", pts["p27"], pts["p26"]),
+        ("line", pts["p20"], pts["p26"]),
+        ("line", pts["p26"], pts["p24_1"]),
+        ("line", pts["p26"], pts["p24_2"]),
+        ("line", pts["p24_1"], pts["p25"]),
+        ("line", pts["p24_2"], pts["p25"]),
 
-        ("dash", pts["p5"], pts["p33"]),
-        ("dash", pts["p2"], pts["p32"]),
-        ("dash", pts["p10"], pts["p11"]),
+        ("dash", pts["p6"], pts["p33"]),
+        ("dash", pts["p3"], pts["p32"]),
+        ("dash", pts["p26"], pts["p25"]),
     ]
 
 

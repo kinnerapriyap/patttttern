97fcc9d8cb9f4ded761a0c520cb3fb0d26cc458c
Kinnera Priya Putti <kinnerapriyap@gmail.com>
2026-04-24 21:09:38 +0200

    Remove unnecessary pts

--- close_fitting_bodice/one_dart_front.py
+++ close_fitting_bodice/one_dart_front.py
@@ -6,10 +6,7 @@ from utils.rotation import angle_between, rotate_point
 def get_rotated_one_dart_front_points():
     angle = angle_between([pts["p26"], pts["p27"]], [pts["p26"], pts["p20"]])
 
-    point_side_keys = [
-        "p32", "p33_2", "p24_1", "p27", "p11",
-        "p16", "p14a", "p22a", "p31", "p30",
-    ]
+    point_side_keys = ["p32", "p33_2", "p24_1", "p27", "p14a", "p22a", "p31", "p30"]
     rotated_points = {
         key: tuple(rotate_point(pts[key], pts["p26"], angle))
         for key in point_side_keys
@@ -26,9 +23,7 @@ def get_one_dart_front_shapes():
         ("curve", pts["p20"], pts["p21"], 0.45),
         ("polyline",
          [pts["p21"], pts["p6"], pts["p24_2"], pts["p26"], rotated["p24_1"], rotated["p33_2"], rotated["p32"]]),
-        ("french_curve",
-         [rotated["p11"], rotated["p16"], rotated["p14a"], rotated["p32"], rotated["p22a"], rotated["p31"],
-          rotated["p30"]], 6.0),
+        ("french_curve", [rotated["p14a"], rotated["p32"], rotated["p22a"], rotated["p31"], rotated["p30"]], 6.0),
         ("line", rotated["p27"], rotated["p30"]),
 
     ]
